6e02224facf3e3fcd172e42b4620a80c997cc459
orpon95 <mdorpon95@gmail.com>
2023-10-19 14:14:52 +0600

    added mongodb and connect it

--- index.js
+++ index.js
@@ -2,16 +2,54 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 // middeleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+// id and pass
+// mdorpon95
+// KvXpL33lAyMITtGH
+
+
+// database start
+
+
+
+const uri = "mongodb+srv://mdorpon95:KvXpL33lAyMITtGH@cluster0.ww7s5no.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// database end
+
+app.get("/", (req, res) => {
     res.send("brand server is running")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`brand server is running at port ${port}`)
 })
